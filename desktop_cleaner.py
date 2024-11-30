bf0fb1cb80334ae9e194814dddc165b2dd5292db
Hashem Tareq <118438641+hashemTareq@users.noreply.github.com>
2024-11-30 15:38:07 +0300

    Update desktop_cleaner.py

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -9,7 +9,6 @@ def create_subfolder_if_needed(folder_path, subfolder_name):
 def clean_folder(folder_path):
   for filename in os.listdir(folder_path):
     if os.path.isfile(os.path.join(folder_path, filename)):
-      # print(os.path.join(folder_path, filename))
       file_extension = filename.split('.')[-1].lower()
       if file_extension:
         subfolder_name = f"{file_extension.upper()} files"
